0224cce565ecebf968826008c19a56d9516275a2
JesusContrerasPacheco <jesuscp17.17@gmail.com>
2021-06-20 03:06:24 -0500

    [Change] clean

--- predict.py
+++ predict.py
@@ -5,33 +5,36 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
 import pickle
 
-path = 'D:/UPC/2021 - 1/IA/proyecto/code/'
+path = 'D:/UPC/2021 - 1/IA/proyecto/IA_Recognition_Expressions/'
 
-def returnLocalizedFace(img):
-    face_cascade = cv2.CascadeClassifier(path + 'classifiers/haarcascade_frontalface_default.xml')
-    gray =cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+# def returnLocalizedFace(img):
+#     face_cascade = cv2.CascadeClassifier(path + 'classifiers/haarcascade_frontalface_default.xml')
+#     gray =cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in faces:
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = img[y:y + h, x:x + w]
-    if len(faces) == 0:
-        return img
-    crop_img = img[y:y + h, x:x + w]
-    return crop_img
+#     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+#     for (x, y, w, h) in faces:
+#         # roi_gray = gray[y:y + h, x:x + w]
+#         # roi_color = img[y:y + h, x:x + w]
+#         if len(faces) == 0:
+#             return img
+#         crop_img = img[y:y + h, x:x + w]
+#         return crop_img
 
 
 def getImage(path):
     return cv2.imread(path)
+
+
 def show(img):
     cv2.imshow('im', img)
-    cv2.waitKey(0)
+    # cv2.waitKey(0)
 
 X = []
 y = []
-def read(imagesPath, label):
 
-    for filename in glob.glob(path + "face2/" + imagesPath + '/*.*'):
+def read(imageFolder, label):
+
+    for filename in glob.glob(path + "images/" + imageFolder + '/*.*'):
         win_size = (64, 128)
         img = getImage(filename)
 
@@ -44,17 +47,17 @@ def read(imagesPath, label):
         X.append(hog.transpose()[0])
         y.append(label)
 
-def fromIndexToFeatures(X, indecies):
-    features = []
-    for i in indecies:
-        features.append(X[i])
-    return np.asarray(features)
+# def fromIndexToFeatures(X, indecies):
+#     features = []
+#     for i in indecies:
+#         features.append(X[i])
+#     return np.asarray(features)
 
-def fromIndexToLabels(y, indecies):
-    labels = []
-    for i in indecies:
-        labels.append(y[i])
-    return np.asarray(labels)
+# def fromIndexToLabels(y, indecies):
+#     labels = []
+#     for i in indecies:
+#         labels.append(y[i])
+#     return np.asarray(labels)
 
 def openModal(img):
     image_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -67,7 +70,7 @@ def openModal(img):
         for x,y in landmark[0]:
             cv2.circle(img, (int(x), int(y)), 1, (255, 255, 255), 1)
     
-    cv2.imshow("img", img)
+    show(img)
 
 read('HAPPY',0)
 read('CONTEMPT',1)
@@ -90,8 +93,9 @@ pickle.dump(clf, open(filename, 'wb'))
 
 clf = pickle.load(open(filename, 'rb'))
 
-img = getImage(path + "face2/" + 'testImages/happy1.PNG')
+img = getImage(path + 'testImages/happy2.PNG')
 
+# points 
 openModal(img)
 
 win_size = (64, 128)
@@ -104,7 +108,6 @@ hog = d.compute(img)
 hog = hog.transpose()[0]
 hog = np.asarray(hog)
 print(hog)
-print(classes[clf.predict([hog])[0]])
+print("Result: " + classes[clf.predict([hog])[0]])
 
 cv2.waitKey(0)
-print("fin")
